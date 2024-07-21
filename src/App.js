202c32cf389a653e3332acae7f9af9709b3208bb
tshamugia <shamugiatengo@gmail.com>
2024-07-21 12:44:36 +0400

    final ready to commit, added images from cloudFront

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './App.scss';
 
@@ -12,14 +12,64 @@ function App() {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user.user);
 
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     dispatch(getInfoFromDB());
   }, [user.balance]);
 
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/api/images/');
+        if (response.ok) {
+          const data = await response.json();
+          setImages(data);
+        } else {
+          throw new Error('Server Error!');
+        }
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchImages();
+  }, [user.card_id]);
+
+
+
   return (
     <>
       <h1 className="header">Loyalty Card</h1>
+      <div className="images-container">
+          {loading && <p>Loading images...</p>}
+          {error && <p>Error: {error}</p>}
+          {images.length > 0 && (
+            <div className="images-grid">
+              {images.map((image, index) => (
+                <img
+                  key={index}
+                  src={image.image} // Adjust the property name if necessary
+                  alt={image.name}
+                  className="image"
+                />
+              ))}
+            </div>
+          )}
+        </div>
       <div className="app">
+    
+     
+
+
+
+
+
+
         <div className="container">
           <Display />
           <Buttons />
